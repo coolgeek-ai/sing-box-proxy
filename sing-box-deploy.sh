8005f219697dfa45fd1d9015d41372d993c7a913
coolgeek <coolgeek2019@gmail.com>
2025-08-12 05:11:09 -0400

    Update sing-box-deploy.sh

--- sing-box-deploy.sh
+++ sing-box-deploy.sh
@@ -90,7 +90,7 @@ install_singbox() {
     esac
     
     # Download and install
-    SINGBOX_TAR="sing-box-${SINGBOX_VERSION}-linux-${SINGBOX_ARCH}.tar.gz"
+    SINGBOX_TAR="sing-box-${SINGBOX_VERSION#v}-linux-${SINGBOX_ARCH}.tar.gz"
     wget "https://github.com/SagerNet/sing-box/releases/download/${SINGBOX_VERSION}/${SINGBOX_TAR}" -O /tmp/${SINGBOX_TAR}
     tar -zxf /tmp/${SINGBOX_TAR} -C /tmp
     cp /tmp/sing-box-${SINGBOX_VERSION}-linux-${SINGBOX_ARCH}/sing-box /usr/local/bin/
